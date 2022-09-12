2287a317c1757299b1b24d839f97dd27601ab5d1
Elizabeth Kennedy <eakennedy.2412@gmail.com>
2022-09-11 17:52:36 -0700

    fixed input button issue

--- app.js
+++ app.js
@@ -1,52 +1,30 @@
 "use strict";
-
-
 console.log("Hello Fetch API and Async/Await!");
 
 // Exercise 1 Section
+/*Inspect and select elements*/
+
 console.log("EXERCISE 1:\n==========\n");
-
-const GIPHY_KEY = "0pOUjDOVnLFJGtCN70a7ZrMlzbOrwASx";
+//Program State 
 const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate";
-
-// EDOM Selection 
-const form = document.querySelector("form");
-const search = document.querySelector("#search");
-const img = document.querySelector("img");
-
-//Event Listeners
-form.addEventListener("submit", fetchGiphy);
-   
-async function fetchGiphy(event) {
-    event.preventDefault();
-
-    let searchValue = search.value;
-
-try {
-    let response = await fetch(
-        GIPHY_URL + "?api_key=" + GIPHY_KEY + "&s=" + searchValue
-    );
-    let results = await response.json();
-
-    console.log(results);
-
-    img.src = results.data.images.original.url;
-} catch (err) {
-    console.error(err);
-}
-}
-
-
-
-
-// fetch(GIPHY_URL + "?api_key=" + GIPHY_KEY + "&s=" + searchValue)
-// .then((response) => response.json())
-// .then((results) => {
-//     console.log(results);
-//     img.src = results.data.images.original.url;
-// })
-
-// .catch((err) => {
-//     console.error(err);
-//     });
-// });
+const GIPHY_KEY = "0pOUjDOVnLFJGtCN70a7ZrMlzbOrwASx";
+//Select Elements
+let feedbackEle = document.querySelector("#feedback");
+let searchInput = document.querySelector("#searchWord");
+let searchBtn = document.querySelector("#submitSearch");
+let gifEle = document.querySelector("#imageContainer > img");
+//Event Handlers
+//let searchBtn = document.getElementById("searchBtn").addEventListener("click");
+searchBtn.addEventListener("click", (event) => {
+    fetch(`${GIPHY_URL}?api_key=${GIPHY_KEY}&s=${searchInput.value}`)
+    .then((res) => res.json())
+    .then((body) => {
+        //show the gif on the dom
+        gifEle.src = body.data.images.original.url;
+    })
+    .catch((err) => {
+        console.error(err);
+//show the error message on the dom
+    feedbackEle.textContent = err.message;
+    });
+});
